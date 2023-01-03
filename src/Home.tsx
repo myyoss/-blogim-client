c327711ba70e8346fa062d718c63e4cda6c2a5a7
YossiAS <93940739+myyoss@users.noreply.github.com>
2023-01-03 19:17:23 +0200

    https://blogim-json-server.onrender.com

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,7 @@ import Spinners from './Spinners'
 import useFetch from './useFetch'
 
 function Home() {
-    const { isPanding, data: blogs, error } = useFetch('http://localhost:8000/blogs/')
+    const { isPanding, data: blogs, error } = useFetch('https://blogim-json-server.onrender.com/blogs/')
 
 
     return (
